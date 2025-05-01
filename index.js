1c370252455dce58fc4cb09940bd8f8072ed92b0
KalpaniKapuge <kalpanikapuge1020@gmail.com>
2025-05-01 18:38:55 +0530

    Modified the index file✏️

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ import userRouter from './routes/userRouter.js';
 import jwt from 'jsonwebtoken';
 import orderRouter from './routes/orderRouter.js';
 import reviewRouter from './routes/reviewRouter.js';
-
+import cors from 'cors';
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 
 app.use(
@@ -51,13 +52,13 @@ mongoose.connect("mongodb+srv://admin:123@cluster0.bukbozs.mongodb.net/?retryWri
 );
 
 
-app.use("/products",productRouter)
+app.use("/api/products",productRouter)
 
-app.use("/users",userRouter)
+app.use("/api/users",userRouter)
 
-app.use("/orders",orderRouter)
+app.use("/api/orders",orderRouter)
 
-app.use("/reviews",reviewRouter)
+app.use("/api/reviews",reviewRouter)
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
